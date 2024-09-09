a0761fd4924ed11409c4ffbe131e892e822d4fdc
Myelin Lennox <118149338+MyelinLennox@users.noreply.github.com>
2024-09-09 19:13:28 +0000

    add tables, chairs, temp convert, adn Triangles

---
+++ TablesAndChairs.java
@@ -0,0 +1,13 @@
+// Call Class
+class TableAndChairs {
+    // Declare main for string printing
+    public static void main(String[] args) {
+        // Once again use too many individual print functions
+        System.out.println("|}                         {|");
+        System.out.println("|                           |");
+        System.out.println("|        -=========-        |");
+        System.out.println("|_____    |]-   -[|    _____|");
+        System.out.println("|----|    |       |    |----|");
+        System.out.println("|    |    !       !    |    |");
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,17 @@
+// Define class
+class Triangle {
+    // Main function (includes neccesary string arguments)
+    public static void main (String[] args) {
+        //Use wayyyyy too many print functions, cheap "hack" but it works
+        System.out.println("        T        ");
+        System.out.println("       TTT       ");
+        System.out.println("      TTTTT      ");
+        System.out.println("     TTTTTTT     ");
+        System.out.println("    TTTTTTTTT    ");
+        System.out.println("   TTTTTTTTTTT   ");
+        System.out.println("  TTTTTTTTTTTTT  ");
+        System.out.println(" TTTTTTTTTTTTTTT ");
+        System.out.println("TTTTTTTTTTTTTTTTT");
+    }
+
+}
